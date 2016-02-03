82d8e7b66942bcdea79b7c4621d18795dc96f06d
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2016-02-03 13:55:05 -0500

    Added sigar lib files to a zip for easy extraction into a lib file to the users working directory.

--- src/main/java/com/mohorovich/mitchell/rssb/Main.java
+++ src/main/java/com/mohorovich/mitchell/rssb/Main.java
@@ -6,12 +6,19 @@ package com.mohorovich.mitchell.rssb; /**
 import com.mohorovich.mitchell.rssb.pollers.CPUPoller;
 import com.mohorovich.mitchell.rssb.pollers.MemPoller;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import static spark.Spark.*;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		writeSigarLibToJavaLibPath();
+
 		CPUPoller cpuPoller = new CPUPoller();
 		cpuPoller.start();
 		get("/cpu", (req, res) -> cpuPoller.getCpu());
@@ -29,4 +36,30 @@ public class Main {
 		get("/network/upload", (req, res) -> "network upload");
 		get("/network/download", (req, res) -> "network download");
 	}
+
+	public static void writeSigarLibToJavaLibPath() throws Exception {
+		File libraryDirectory = new File(System.getProperty("user.dir").concat("/lib"));
+		if(!libraryDirectory.exists()) {
+			if(!libraryDirectory.mkdir()) {
+				throw new Exception("Could not make lib directory.");
+			}
+		}
+		System.setProperty("user.dir", System.getProperty("user.dir").concat("/lib"));
+		System.setProperty("java.library.path", System.getProperty("user.dir"));
+
+		InputStream in = Main.class.getResourceAsStream("/sigar-lib.zip");
+		ZipInputStream zin = new ZipInputStream(in);
+		ZipEntry ze;
+		while ((ze = zin.getNextEntry()) != null) {
+			System.out.println("Unzipping " + ze.getName());
+			FileOutputStream fOut = new FileOutputStream(System.getProperty("java.library.path") + "/" + ze.getName());
+			for (int c = zin.read(); c != -1; c = zin.read()) {
+				fOut.write(c);
+			}
+			zin.closeEntry();
+			fOut.close();
+		}
+		zin.close();
+	}
+
 }
